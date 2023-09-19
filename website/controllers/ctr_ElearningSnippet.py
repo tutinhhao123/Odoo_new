3af7b149fb00016a4450f4a07d3470ccb8bc4842
Tran Tuan Anh <tutinhhao@gmail.com>
2023-09-20 00:49:24 +0700

    first commit

---
+++ website/controllers/ctr_ElearningSnippet.py
@@ -0,0 +1,11 @@
+from odoo import http
+
+
+class ElearningSnippet(http.Controller):
+    @http.route(['/latest_elearning_courses'], type="json", auth="public", website=True, methods=['POST'])
+    def all_courses(self):
+        courses = http.request.env['slide.channel'].search_read(
+            [('website_published', '=', True)], ['name', 'image_1920', 'id'],
+            order='create_date desc', limit=10)
+
+        return courses
